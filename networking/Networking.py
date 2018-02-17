e34709113056d65d49715d27b21c94cd507ba240
Beck <physicalc123@gmail.com>
2018-02-17 16:46:09 -0600

    Networking

---
+++ networking/Networking.py
@@ -0,0 +1,14 @@
+import requests
+import json
+# class Miner:
+#     def __init__():
+#
+#     def broadcast_block_found(block):
+#         print('test')
+#
+class Public:
+    def __init__(self, public_ledger_addr):
+        self.public_ledger_addr = public_ledger_addr
+    def return_blockchain(self):
+        data = requests.post(self.public_ledger_addr + "/blockchain")
+        return data.json()
---
+++ networking/Public.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import json
+from flask import jsonify
+
+public = Flask(__name__)
+
+public_nodes = {}
+
+with open("nodes.json", "r") as json_file:
+    data = json.load(json_file)
+    public_nodes = data
+
+@public.route("/nodes")
+def return_nodes():
+    return json.dumps(public_nodes)
+
+#Matthew Look Here
+@public.route("/blockchain", methods=['POST'])
+def return_blockchain():
+    return jsonify(chain=['nothing'])
