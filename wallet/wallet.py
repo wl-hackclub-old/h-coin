1af099dea38c2aabbbb7c3973c04aaef26989046
TechBug2012 <mattbstanciu@gmail.com>
2018-02-17 18:48:13 -0600

    added wallet info
    Co-Authored-By: beck3k <beck3k@users.noreply.github.com>
    Co-Authored-By: zedhatool <zedhatool@users.noreply.github.com>

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,5 +1,10 @@
 
 class Wallet:
-
+    balance = 0
+    address = ""
     def send(self, amount, address):
-        print ("artificially flavoured ass :)")
+        balance = balance-amount
+
+class Log:
+    def __init__(self, this_address, proof):
+        this_address =
