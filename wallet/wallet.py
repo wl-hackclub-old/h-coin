78f52c9fd38356957bed78575e5348cebcb6a336
zedhatool <zedhatool@gmail.com>
2018-02-17 21:33:00 -0500

    Wallet fixed

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -5,19 +5,34 @@ class Wallet:
     def send(self, amount, address):
         balance = balance-amount
 # you hooligans!
-class Log:
-    def __init__(self, this_address, proof):
-        this_address = open("walletinfo.txt", w)
-        this_address.write("Wallet Key: " + get_wallet_address())
-        this_address.write("Public Key: " + get_public_key())
-        this_address.write("Private Key: " + get_private_key())
+class LogWallet:
+    def __init__(self):
+        generator = WalletGenerator()
+        path = './walletinfo.txt'
+        this_address = open(path, 'w')
+        this_address.write("Wallet Key: " + generator.get_wallet_address())
+        this_address.write("\nPublic Key: " + generator.get_public_key())
+        this_address.write("\nPrivate Key: " + generator.get_private_key())
         this_address.close()
-    def get_address(this_address):
-        this_address = open("walletinfo.txt", r)
-        print this_address.readline(1)
-    def get_public(this_address):
-        this_address = open("walletinfo.txt", r)
-        print this_address.readline(2)
-    def get_private(this_address):
-        this_address = open("walletinfo.txt", r)
-        print this_address.readline(3)
+    def get_address(this_address, path):
+        path = './walletinfo.txt'
+        this_address = open("walletinfo.txt", 'r')
+        lines = this_address.readlines()
+        return lines[0]
+        this_address.close()
+    def get_public(this_address, path):
+        path = './walletinfo.txt'
+        this_address = open("walletinfo.txt", 'r')
+        lines = this_address.readlines()
+        return lines[1]
+        this_address.close()
+    def get_private(this_address, path):
+        path = './walletinfo.txt'
+        this_address = open(path, 'r')
+        lines = this_address.readlines()
+        return lines[2]
+        this_address.close()
+l = LogWallet()
+print(l.get_public)
+print(l.get_address)
+print(l.get_private)
